4d078e2f3047fe2d1fda904334237245e4caab39
1 <sergey.trbv@yandex.ru>
2022-08-20 13:59:44 +0400

    Третий коммит (Второй не переименовал :с)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет МИР!!!ТЫ КОВАРЕН!!!");
+        System.out.println("Привет МИР!!!ТЫ КОВАРЕН!!!!");
     }
 }
